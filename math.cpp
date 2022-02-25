ac480a4bd947903fa09dcdfe0e16d836de5abdb0
Yiyun Qin <yiyun.qin@mths.ca>
2022-02-25 19:53:27 +0000

    circle math

---
+++ math.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Yiyun Qin All rights reserved
+//
+// Created by Yiyun Qin
+// Created at February 2022
+// This is a "Hello World" program with proper style
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "If a circle has a radius of 15 mm:" << std::endl;
+    std::cout <<  std::endl;
+    std::cout << "Area is " << (M_PI * pow(15 , 2)) << " mm²." << std::endl;
+    std::cout << "Perimetre is " << 2 * M_PI * 15 << " mm." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
